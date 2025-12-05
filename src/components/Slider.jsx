f81e5bdc33feec9672d09d4147fba2dcabe8e73a
Shivay Rathore <shivay0225@gmail.com>
2025-12-05 21:58:59 +0530

    update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -18,7 +18,7 @@ const MySlider = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
@@ -33,6 +33,13 @@ const MySlider = () => {
       },
       {
         breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+       {
+        breakpoint: 350,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
