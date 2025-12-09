f4c7be0b16b528ef079a5ab1c0197ae3cd601064
Shivay Rathore <shivay0225@gmail.com>
2025-12-10 04:35:12 +0530

    udpate

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -95,7 +95,7 @@ const Header  = () =>{
     </div>
   </nav>
   <div className="container-fluid  d-flex justify-content-center align-items-center">
-  <div className=" container cn2  d-flex justify-content-center ms-lg-5 ms-2 align-items-center">
+  <div className=" container cn2  d-flex justify-content-center ms-lg-5  align-items-center">
     <div className="booking-bar d-lg-flex  py-2  py-4 d-block  justify-content-between align-items-center">
       <div className="booking-item my-lg-0 my-3 ">
         <div className="list-box-1 d-flex px-3 py-lg-0 py-2 px-3 align-items-center">
